bfcba379229ff3470e9493201db6c25c4e522584
Hamdi Mohamed <31364936+x0911@users.noreply.github.com>
2021-05-27 12:05:34 +0200

    Back to static site generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,10 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true, // false
+  ssr: false, // false
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'server', // static
+  target: 'static', // static
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
