25d4f2f85bb2373fcebef7c567be226e684be992
Hamdi Mohamed <maracap.x911@gmail.com>
2021-06-07 01:44:48 +0000

    Fixed update-user-data api route

--- api/update-user-data.js
+++ api/update-user-data.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const router = Router()
 
-router.use('/get-user-data', (req, res) => {
+router.use('/update-user-data', (req, res) => {
   res.json({
     msg: 'User has been updated!',
     updatedKeys: req.keys && Array.isArray(req.keys) ? req.keys : null,
